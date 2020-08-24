acd58ebc63f7884b1f2e1ca118579927f49b9895
NicolasSantoni <57455849+NicolasSantoni@users.noreply.github.com>
2020-08-24 00:31:08 -0300

    Documentando

--- NicolasJS63-B/usuarios.php
+++ NicolasJS63-B/usuarios.php
@@ -1,5 +1,5 @@
 <?php
-
+/** Página completa para se listar os usuários, verificando se o usuário atual está logado ou não, se não, ele será levado para um formulário para que o mesmo faça login */
 	include "config.php";
 	include HOME_DIR."view/tema/header.php";
 	include HOME_DIR."view/tema/sessao.php";
